0a1032aae67e352328d328645996851f79e43282
MasterLaplace <guillaume.papineau@epitech.eu>
2024-03-28 19:54:18 +0100

    feat(LplKernel): add initial setup, generate iso and emulate on Qemu

---
+++ src/hello_world.c
@@ -0,0 +1,21 @@
+/*
+** LAPLACE PROJECT, 2024
+** Laplace [LplKernel]
+** Author: MasterLaplace
+** Created: 2023-10-13
+** File description:
+** hello_world
+*/
+
+#ifdef __cplusplus
+    #include <utility>
+    #include <type_traits>
+#endif
+
+#include <stdint.h>
+#include <stddef.h>
+
+int main(void)
+{
+    return 0;
+}
