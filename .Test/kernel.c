fa434d5b1c65aa3b6e23e76099ad169a3c270b97
MasterLaplace <guillaume.papineau@epitech.eu>
2024-04-29 20:06:08 +0200

    delete(.Test): delete all experimentation

--- .Test/kernel.c
+++
@@ -1,8 +0,0 @@
-void kernel_main() {
-    const char* str = "Hello, World";
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
-
-    for(int i = 0; str[i] != '\0'; ++i) {
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
-    }
-}
